fe0ee10b5825f2d7dc36d7de3a74c4d1829c66b9
Ruby Cannon <rubycannon1990@gmail.com>
2024-06-13 09:49:23 -0400

    Auto-commit on file save

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h2>
+        <p>
           Input ingredients and we'll tell you what drinks you can make!
-        </h2>
+        </p>
 
       </header>
     </div>
