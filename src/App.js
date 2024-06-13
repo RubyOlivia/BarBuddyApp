f8eebd437db90ae5ebaea60f28cc138d14c2dc42
Ruby Cannon <rubycannon1990@gmail.com>
2024-06-13 09:24:22 -0400

    Auto-commit on file save

--- src/App.js
+++ src/App.js
@@ -6,17 +6,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <h1>
           Input ingredients and we'll tell you what drinks you can make!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Bar Buddy App !!!!
-        </a>
+        </h1>
+
       </header>
     </div>
   );
