e3a195aacde8432f54b333ebf4878ed18ff66e08
Ruby Cannon <rubycannon1990@gmail.com>
2024-06-14 23:50:14 -0400

    Auto-commit on file save

--- src/App.js
+++ src/App.js
@@ -1,18 +1,1 @@
-import logo from './assets/cokctail.png';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Input ingredients and we'll tell you what drinks you can make!
-        </p>
-
-      </header>
-    </div>
-  );
-}
-
-export default App;
